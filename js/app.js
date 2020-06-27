5320fa72302e7100e7c6473edaedc7cef9db829e
Shubham <shubhammurteli19@gmail.com>
2020-06-27 21:52:18 +0530

    Updated JS Functionality and some asthetic changes

--- js/app.js
+++ js/app.js
@@ -1,28 +1,34 @@
 const display = document.getElementById("text");
-const github = "https://github.com/shubhammurteli";
-const twitter = "https://twitter.com/Shubz_12";
-const insta = "https://www.instagram.com/19_shubhamm/?hl=en";
-const codepen = "https://codepen.io/19_cipher";
-const gmail = "shubhammurteli19@gmail.com";
-const logo = document.querySelectorAll(".contact-logo");
+
+const text = ["https://twitter.com/Shubz_12", "https://www.instagram.com/19_shubhamm/?hl=en", "https://codepen.io/19_cipher", "https://github.com/shubhammurteli", "shubhammurteli19@gmail.com"]
+
+const logo = document.querySelectorAll(".contact-logo"); // Gives an array of the elements with class ".contact-logo"
 
 logo.forEach(function (pic) {
   pic.addEventListener("mouseover", function(e) {
-    const image = e.currentTarget.id; //gives the id of the element which triggered the event
-    if(image == "twitter"){
-      display.innerHTML = twitter;
-    }
-    else if(image == "instagram"){
-      display.innerHTML = insta;
-    }
-    else if(image == "codepen"){
-      display.innerHTML = codepen;
-    }
-    else if(image == "github"){
-      display.innerHTML = github;
-    }
-    else{
-      display.innerHTML = gmail;
-    }
+    const clicked = e.currentTarget.id; //gives the id of the element which triggered the event
+  if (clicked == "twitter") {
+    cleartext(text[0]);
+  }
+  else if (clicked == "instagram") {
+      cleartext(text[1]); 
+  }
+  else if (clicked == "codepen") {
+      cleartext(text[2]);
+  }
+  else if (clicked == "github") {
+      cleartext(text[3]);
+  }
+  else {
+      cleartext(text[4]);
+  }
   });
 });
+
+function cleartext(a) {
+  clearTimeout(clear);
+  display.innerHTML = a;
+  var clear = setTimeout(() => {
+      display.innerHTML = "Hover again";
+  }, 5000);
+}
